faf76dec001334ff7be372627ddaafc4055821b3
Denis <wolf1323@mail.ru>
2023-03-05 06:38:16 +0300

    Создал файл запуска программы.

---
+++ main.py
@@ -0,0 +1,14 @@
+from utils import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
+
+
+Item.pay_rate = 0.8  # устанавливаем новый уровень цен
+item1.apply_discount()
+print(item1.price)
+print(item2.price)
+print(Item.all)
